29d44371631caf7070ad0572a9e86d2d4bd5a6b6
karan886 <karansachdev886@gmail.com>
2018-10-06 20:21:45 -0700

    AG-4: Setup basic grid spawning.

--- game.lua
+++ game.lua
@@ -3,10 +3,14 @@ local composer = require( "composer" )
 local scene = composer.newScene()
  
 --some dimensions
-local height = display.actualContentHeight
-local width = display.actualContentWidth
+local actualHeight = display.actualContentHeight
+local actualWidth = display.actualContentWidth
+
 local centerX = display.contentCenterX
 local centerY = display.contentCenterY
+
+local width = display.contentWidth
+local height = display.contentHeight
 
 --scene garbage for objects that are not latched on to the scene
 local bin = {}
@@ -20,7 +24,7 @@ function ParallaxScroll(object, options)
     --initialize options
     --the point where a parallax object starts and ends on the screen (ie. it ends when it wraps around)
     local ObjectStart = { x = centerX, y = centerY - 200 } or options.start
-    local ObjectEnd = { x = centerX, y = height} or options.exit
+    local ObjectEnd = { x = centerX, y = actualHeight} or options.exit
     -- the speed at which the parallax object's location updates in the scene (this is usually measured in pixels per frame in this case)
     local Speed = 1 or options.speed
 
@@ -31,10 +35,35 @@ function ParallaxScroll(object, options)
         object.y = object.y + Speed
     else
         if ( object ~= nil ) then
-            object.x = math.random(0, width)
+            object.x = math.random(0, actualWidth)
             object.y = ObjectStart.y
         end
     end  
+end
+
+function createGrid(grid)
+
+   local gridXPos = (width - (grid.size * grid.blockSize + grid.size * grid.offsetX) / 2) - centerX
+   local gridYPos = (height - (grid.size * grid.blockSize + grid.size * grid.offsetY) / 2) - centerY
+
+  for i=1,grid.size do
+    for j=1,grid.size do
+        local block = display.newRect(100, 100, grid.blockSize, grid.blockSize)
+        block.x = gridXPos + i * (grid.blockSize + grid.offsetX) - grid.blockSize/2 - grid.offsetX
+        block.y = gridYPos + j * (grid.blockSize + grid.offsetY) - grid.blockSize/2 - grid.offsetY
+
+        print("spawned block: "..block.x.." , "..block.y)              
+    end
+  end
+end
+
+function spawnGrid()
+   createGrid({
+      size = 3,
+      blockSize = 30,
+      offsetX = 5,
+      offsetY = 5
+    })
 end
 
 
@@ -59,6 +88,7 @@ function scene:show( event )
  
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
+        spawnGrid()
     end 
 end
 
