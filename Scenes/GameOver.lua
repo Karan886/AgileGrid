e1e42758b79114be02859a62bcd709c5d34fc22b
= <karan_sachdev_2@sfu.ca>
2019-09-01 15:45:20 -0700

    Master: setup gameover modal and transitions.

--- Scenes/GameOver.lua
+++ Scenes/GameOver.lua
@@ -2,6 +2,7 @@ local composer = require("composer")
 local scene = composer.newScene()
 
 local blur = require "Modules.Blur"
+local ui = require "Modules.UI"
 
 --some dimensions
 local actualHeight = display.actualContentHeight
@@ -11,6 +12,8 @@ local centerY = display.contentCenterY
 
 -- ui layers
 local blurLayer = display.newGroup()
+local backgroundLayer = display.newGroup()
+local gameStatsLayer = display.newGroup()
 
 function scene:create( event )
     print("switched to Game Over scene....")
@@ -23,18 +26,55 @@ function scene:create( event )
         ymax = actualHeight,
     })
     blurLayer: insert(blurOverlay)
-   
-    sceneGroup:insert(blurLayer)  
+    sceneGroup:insert(blurLayer) 
 end
  
 function scene:show( event )
     local sceneGroup = self.view
+    local params = event.params
+    local gameStats = params.gameData
 
     if (event.phase == "will") then
 
     elseif (event.phase == "did") then
+        local modal = display.newImage("Images/UI/ModalSmall.png", centerX, centerY)
+        modal.alpha = 0
+        backgroundLayer:insert(modal)
+
+        local gameoverTitle = display.newText("GameOver", modal.x, 0, "Fonts/BigBook-Heavy", 20)
+        gameoverTitle.y = modal.y - modal.height / 2 + gameoverTitle.height / 2
+        gameoverTitle.alpha = 0
+        gameoverTitle:setFillColor(0.8, 0, 0)
+        backgroundLayer:insert(gameoverTitle)
+
+         -- Ease in the modal using transition
+        transition.to(modal, {
+            alpha = 1.0,
+            time = 1500,
+            onComplete = function()
+                gameoverTitle.alpha = 1.0
+                ui.displayGameStats({
+                    data = gameStats,
+                    layer = gameStatsLayer,
+                    x = centerX,
+                    y = centerY,
+                    width = modal.width - 10,
+                    height = modal.height - 20,
+                    yOffsetFactor = 1.5,
+                    fontSize = 20,
+                    font = "Fonts/Futura-Bold",
+                    alias = {
+                        tripleMatches = "Triple Matches:",
+                        doubleMatches = "Double Matches:",
+                        matches = "Matches:"
+                    }
+                })
+            end
+        })
 
     end
+    sceneGroup:insert(backgroundLayer)
+    sceneGroup:insert(gameStatsLayer)
 end
  
 function scene:hide( event )
